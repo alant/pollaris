35f204dd89dc39487ae380675fafa10a8adb623b
nataliechen <nataliechen@coupang.com>
2018-06-23 13:14:41 -0700

    add attributes to team

--- contract.js
+++ contract.js
@@ -1,88 +1,99 @@
 'use strict';
 
 var Team = function(obj) {
-  this.parse(obj);
+    this.parse(obj);
 };
 
 Team.prototype = {
-  toString: function() {
-    return JSON.stringify(this);
-  },
+    toString: function() {
+        return JSON.stringify(this);
+    },
 
-  parse: function(text) {
-    if (text) {
-      var obj = JSON.parse(text);
-      this.players = obj.players;
-      this.scores = obj.scores;
-      this.rewardPool = obj.rewardPool;
-      this.approvals = obj.approvals;
-      this.gameOver = obj.gameOver;
-      this.timeStamp = Obj.timeStamp;
-    } else {
-      this.players = [];
-      this.scores = [];
-      this.rewardPool = new BigNumber(0);
-      this.approvals = [];
-      this.gameOver = '0';// 0 game is still live, 1 game is over, 2 contestants approve, 3 contestants do not approve
-      this.timeStamp = '0'; // 24 hours auto approval
+    parse: function(text) {
+        if (text) {
+            var obj = JSON.parse(text);
+            this.players = obj.players;
+            this.scores = obj.scores;
+            this.avgScore = obj.avgScore;
+            this.reward = obj.reward;
+            // id
+            // description
+            // url
+
+        } else {
+            this.players = [];
+            this.scores = [];
+            this.avgScore = new BigNumber(0);
+            this.reward = new BigNumber(0);
+
+
+        }
     }
-  }
 };
 
 
 var Hacker = function(obj) {
-  this.parse(obj);
+    this.parse(obj);
 };
 
 Hacker.prototype = {
-  toString: function() {
-    return JSON.stringify(this);
-  },
+    toString: function() {
+        return JSON.stringify(this);
+    },
 
-  parse: function(text) {
-    if (text) {
-      var obj = JSON.parse(text);
-      this.reputation = obj.reputation;
-    } else {
-      this.reputation = 3; // initial reputation after 3 strikes you're 0 then it's bad
+    parse: function(text) {
+        if (text) {
+            var obj = JSON.parse(text);
+            this.reputation = obj.reputation;
+            // name
+            // contact
+            // address []
+        } else {
+            this.reputation = 3; // initial reputation after 3 strikes you're 0 then it's bad
+        }
     }
-  }
 };
 
 var NebHackathonContract = function() {
-  LocalContractStorage.defineMapProperties(this, {
-    teams: {
-      parse: function(value) {
-        return new Team(value);
-      },
-      stringify: function(o) {
-        return o.toString();
-      }
-    },
-    hackers: {
-      parse: function(value) {
-        return new Hacker(value);
-      },
-      stringify: function(o) {
-        return o.toString();
-      }
-    }
-  });
-  LocalContractStorage.defineProperties(this, {
-    depositRequirement: 0,
-    totalShortnameCount: 0,
-    sayHack: ''
-  });
+    LocalContractStorage.defineMapProperties(this, {
+        teams: {
+            parse: function(value) {
+                return new Team(value);
+            },
+            stringify: function(o) {
+                return o.toString();
+            }
+        },
+        hackers: {
+            parse: function(value) {
+                return new Hacker(value);
+            },
+            stringify: function(o) {
+                return o.toString();
+            }
+        }
+    });
+    LocalContractStorage.defineProperties(this, {
+        depositRequirement: 0,
+        totalShortnameCount: 0,
+        sayHack: ''
+    });
 }
 
 NebHackathonContract.prototype = {
-  init: function() {
-     this.sayHack = 'awesome hackathon';
-  },
-  get: function() {
-    var result = this.sayHack;
-    return result;
-  }
+    init: function() {
+        this.sayHack = 'awesome hackathon';
+    },
+    get: function() {
+        var result = this.sayHack;
+        return result;
+    },
+    getTeamReward: function(_id) {
+        var team = this.teams.get(_id);
+        var result = team.reward;
+        return
+    }
+
 };
 
 module.exports = NebHackathonContract;
