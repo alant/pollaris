2f68433ce27f16ff454c64029d00a62da20a21bd
alant <alan.htang@gmail.com>
2018-06-24 17:11:01 -0700

    demo version thank you everyone <3

--- config.js
+++ config.js
@@ -8,4 +8,6 @@
 // const contractAddr = "n1gCAiX8isG2drJ6rr8PWVvK6X5KWY9fBKi";
 // 13:33
 // const contractAddr = "n1nF8PTXGr2nvJURtkNo5uNAiZRHVru528d";
-const contractAddr = "n1fHPJdWvBdpkmJY9SWXEYyboRDyjB18vdH";
+// 16:07
+// const contractAddr = "n1fHPJdWvBdpkmJY9SWXEYyboRDyjB18vdH";
+const contractAddr = "n1vJ1Ef2Q9KacdaMDrutKVZWkrpEKvhDz68";
